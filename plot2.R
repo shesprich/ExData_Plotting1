c6abe404235c410afe18b0ddbf0b3b03b72c029b
shesprich <shane.hesprich@marquette.edu>
2022-08-22 11:40:38 -0500

    Initial commit of plot2.R

---
+++ plot2.R
@@ -0,0 +1,12 @@
+# Load script for downloading and loading data
+source("getData.R")
+
+# Use load_data function sourced from getData.R to load data
+energyData <- load_data()
+
+ylab <- "Global Active Power (kilowatts)"
+
+
+png(file = "plot2.png")
+plot(energyData$Global_active_power, ylab = ylab, xlab = "", type = "l")
+dev.off()
